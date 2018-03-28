3c7cb893d747a8dd34fc921bb553b9d15f56b648
Tuomas Nylund <tunylund@gmail.com>
2018-03-28 19:46:43 +0100

    add controls

---
+++ test/controls-test.mjs
@@ -0,0 +1,158 @@
+import jsdom from 'jsdom'
+import Controls from './../lib/controls.mjs'
+import tap from 'tap'
+import { xyz } from './../lib/xyz.mjs'
+
+const { window } = (new jsdom.JSDOM(``, { }))
+
+function key (cs, fn) {
+  cs.map(c => window.document.body.dispatchEvent(new window.KeyboardEvent('keydown', {code: c})))
+  fn()
+  cs.map(c => window.document.body.dispatchEvent(new window.KeyboardEvent('keyup', {code: c})))
+}
+
+function touch (pageX, pageY, fn) {
+  window.document.body.dispatchEvent(new window.TouchEvent('touchstart', {changedTouches: [{pageX, pageY}]}))
+  fn()
+  window.document.body.dispatchEvent(new window.TouchEvent('touchend', {changedTouches: [{pageX, pageY}]}))
+}
+
+tap.test('controls', tests => {
+  const controls = new Controls(window)
+  tap.notOk(controls.left)
+  tap.notOk(controls.up)
+  tap.notOk(controls.right)
+  tap.notOk(controls.down)
+  
+  tests.test('←', t => {
+    key(['ArrowLeft'], () => {
+      t.ok(controls.left)
+      t.deepEqual(controls.dir, xyz(-1, 0, 0))
+    })
+    t.notOk(controls.left)
+    t.end()
+  })
+
+  tests.test('↖︎', t => {
+    key(['ArrowLeft', 'ArrowUp'], () => {
+      t.ok(controls.leftup && controls.upleft)
+      t.deepEqual(controls.dir, xyz(-0.7071, 0.7071))
+    })
+    t.notOk(controls.leftup && controls.upleft)
+    t.end()
+  })
+
+  tests.test('↑', t => {
+    key(['ArrowUp'], () => {
+      t.ok(controls.up)
+      t.deepEqual(controls.dir, xyz(0, 1, 0))
+    })
+    t.notOk(controls.up)
+    t.end()
+  })
+
+  tests.test('↗', t => {
+    key(['ArrowUp', 'ArrowRight'], () => {
+      t.ok(controls.upright && controls.rightup)
+      t.deepEqual(controls.dir, xyz(0.7071, 0.7071, 0))
+    })
+    t.notOk(controls.upright && controls.rightup)
+    t.end()
+  })
+
+  tests.test('→', t => {
+    key(['ArrowRight'], () => {
+      t.ok(controls.right)
+      t.deepEqual(controls.dir, xyz(1, 0, 0))
+    })
+    t.notOk(controls.right)
+    t.end()
+  })
+
+  tests.test('↘', t => {
+    key(['ArrowRight', 'ArrowDown'], () => {
+      t.ok(controls.rightdown && controls.downright)
+      t.deepEqual(controls.dir, xyz(0.7071, -0.7071, 0))
+    })
+    t.notOk(controls.rightdown && controls.downright)
+    t.end()
+  })
+
+  tests.test('↓', t => {
+    key(['ArrowDown'], () => {
+      t.ok(controls.down)
+      t.deepEqual(controls.dir, xyz(0, -1, 0))
+    })
+    t.notOk(controls.down)
+    t.end()
+  })
+
+  tests.test('↙', t => {
+    key(['ArrowDown', 'ArrowLeft'], () => {
+      t.ok(controls.downleft && controls.leftdown)
+      t.deepEqual(controls.dir, xyz(-0.7071, -0.7071, 0))
+    })
+    t.notOk(controls.downleft && controls.leftdown)
+    t.end()
+  })
+
+  controls.detach()
+  tests.end()
+})
+
+tap.test('touch', tests => {
+  const controls = new Controls(window)
+  const w2 = window.innerWidth / 2,
+        h2 = window.innerHeight / 2
+
+  tests.test('←', t => {
+    touch(w2 - 1, h2, () => t.ok(controls.left))
+    t.notOk(controls.left)
+    t.end()
+  })
+
+  tests.test('↖︎', t => {
+    touch(w2 - 1, h2 - 1, () => t.ok(controls.leftup && controls.upleft))
+    t.notOk(controls.leftup && controls.upleft)
+    t.end()
+  })
+
+  tests.test('↑', t => {
+    touch(w2, h2 - 1, () => t.ok(controls.up))
+    t.notOk(controls.up)
+    t.end()
+  })
+
+  tests.test('↗', t => {
+    touch(w2 + 1, h2 - 1, () => t.ok(controls.upright && controls.rightup))
+    t.notOk(controls.upright && controls.rightup)
+    t.end()
+  })
+
+  tests.test('→', t => {
+    touch(w2 + 1, h2, () => t.ok(controls.right))
+    t.notOk(controls.right)
+    t.end()
+  })
+
+  tests.test('↘', t => {
+    touch(w2 + 1, h2 + 1, () => t.ok(controls.rightdown && controls.downright))
+    t.notOk(controls.rightdown && controls.downright)
+    t.end()
+  })
+
+  tests.test('↓', t => {
+    touch(w2, h2 + 1, () => t.ok(controls.down))
+    t.notOk(controls.down)
+    t.end()
+  })
+
+  tests.test('↙', t => {
+    touch(w2 - 1, h2 + 1, () => t.ok(controls.downleft && controls.leftdown))
+    t.notOk(controls.downleft && controls.leftdown)
+    t.end()
+  })
+
+  controls.detach()
+  tests.end()
+})
--- test/xyz-test.mjs
+++ test/xyz-test.mjs
@@ -1,8 +1,12 @@
 import tap from 'tap'
-import { xyz } from './../lib/xyz.mjs'
+import { xyz, vector2 } from './../lib/xyz.mjs'
 
 const a = xyz(1, 1, 1)
 const b = xyz(2, 2, 2)
+
+function radians(angle) {
+  return angle * (Math.PI / 180)
+}
 
 tap.test('should add', test => {
   test.deepEqual(a.add(b), xyz(3, 3, 3))
@@ -19,5 +23,41 @@ tap.test('should be immutable', test => {
   const b = a.add(xyz(-1, -1))
   test.deepEqual(a, xyz(1, 1, 1))
   test.deepEqual(b, xyz(0, 0, 1))
+  test.end()
+})
+
+tap.test('should know angles', test => {
+  test.test('0', t => {
+    const a = vector2(0, 1)
+    t.equal(a.x, 1)
+    t.equal(a.y, 0)
+    t.equal(a.angle, 0)
+    t.equal(a.radian, 0)
+    t.end()
+  })
+  test.test('45', t => {
+    const a = vector2(Math.PI * 1/4, 1)
+    t.equal(a.x, 0.7071)
+    t.equal(a.y, 0.7071)
+    t.equal(a.angle, 45)
+    t.equal(a.radian, Math.PI / 4)
+    t.end()
+  })
+  test.test('225', t => {
+    const a = vector2(Math.PI * 5/4, 1)
+    t.equal(a.x, -0.7071)
+    t.equal(a.y, -0.7071)
+    t.equal(a.angle, 225)
+    t.equal(a.radian, Math.PI * 5/4)
+    t.end()
+  })
+  test.test('-45', t => {
+    const a = vector2(Math.PI * -1/4, 1)
+    t.equal(a.x, 0.7071)
+    t.equal(a.y, -0.7071)
+    t.equal(a.angle, 315)
+    t.equal(a.radian, Math.PI * 7/4)
+    t.end()
+  })
   test.end()
 })
