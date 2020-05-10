ae08ce2471c31299b4104c70b3cc47257bb03d35
Tuomas Nylund <tunylund@gmail.com>
2020-05-10 16:05:23 +0100

    provide an index.ts for library usage

--- lib/draw.d.ts
+++ lib/draw.d.ts
@@ -8,9 +8,8 @@ interface Layer {
     ch: number;
 }
 declare function stopDrawLoop(): void;
-declare function drawing(w: number, h: number, fn: Drawing, win?: Window): HTMLCanvasElement;
 declare function drawingLayer(win?: Window): Layer;
 declare function draw(drawFn: Draw | Drawing, pos?: Position, layer?: Layer, win?: Window): void;
 declare function drawImage(img: HTMLImageElement | HTMLCanvasElement | HTMLVideoElement | ImageBitmap, pos?: Position, layer?: Layer, win?: Window): void;
 declare function isometricDraw(drawFn: Draw, pos: Position, win?: Window): void;
-export { draw, drawImage, drawing, drawingLayer, isometricDraw, stopDrawLoop, Draw };
+export { draw, drawImage, drawingLayer, isometricDraw, stopDrawLoop, Draw };
