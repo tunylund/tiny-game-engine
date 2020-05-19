a9af7ab0a6047cef0b2c0661b0e7ae823ff8378c
Tuomas Nylund <tunylund@gmail.com>
2020-05-20 00:15:09 +0100

    provide animation functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './animation'
 export * from './loop'
 export * from './entity'
 export * from './position'
---
+++ test/animation.test.ts
@@ -0,0 +1,12 @@
+import { linear } from "../src/animation"
+
+describe('animation', () => {
+  describe('linear', () => {
+    it('should calculate the next value in linear scale', () => {
+      expect(linear(0, 0, 3, 3000, 1, 1)).toEqual(1)
+    })
+    it('should not go over the target value', () => {
+      expect(linear(0, 3, 3, 3000, 1, 1)).toEqual(3)
+    })
+  })
+})
